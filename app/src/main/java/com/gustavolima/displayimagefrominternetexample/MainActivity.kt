9e3a89f2673cae430dc28c41bc3ef5f00e3f2120
Gustavo Lima <gustavo.f12@hotmail.com>
2023-08-07 21:18:14 -0300

    Implemented logic to be able to provide an image link for display.

--- app/src/main/java/com/gustavolima/displayimagefrominternetexample/MainActivity.kt
+++ app/src/main/java/com/gustavolima/displayimagefrominternetexample/MainActivity.kt
@@ -2,10 +2,28 @@ package com.gustavolima.displayimagefrominternetexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.gustavolima.displayimagefrominternetexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private var url = "https://cdn.pixabay.com/photo/2017/12/28/20/34/water-3046088_1280.jpg"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonLoadImage.setOnClickListener {
+
+            if (binding.EditTexturl.text.isEmpty()) {
+                Glide.with(this).load(url).into(binding.image)
+            }else{
+                url = binding.EditTexturl.text.toString()
+                Glide.with(this).load(url).into(binding.image)
+            }
+
+
+        }
+
     }
 }
